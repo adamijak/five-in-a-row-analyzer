3f5e0dd1110289bbae08b0bd0874f6d3b209e7c4
Jakub Adamík <adamik.jakub72@gmail.com>
2023-05-27 16:46:25 +0000

    feat: vite set base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,6 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+	base: "/five-in-a-row-analyzer/",
 	plugins: [sveltekit()]
 });
